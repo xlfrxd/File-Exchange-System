6fe14ca2bb4cb3de1eb0024c8252bbd59a3030c1
xlfrxd <alfred_victoria@dlsu.edu.ph>
2023-11-24 13:15:08 +0800

    Register functionality + error handling + client loop altered

--- Client/Client.java
+++ Client/Client.java
@@ -1,7 +1,6 @@
 package Client;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
@@ -14,91 +13,111 @@ public class Client {
     private static PrintWriter out;
     private static String username = "User";
 
-    
-        
-    
     public static void main(String[] args) {
         String userInput = "";
-        
         String errorString = "";
-        
-        
-        while (true) {
-            try {
-                Scanner scan = new Scanner(System.in);
-                do {
 
-                    System.out.print(username + ": ");
+        Boolean isConnected = false; // for register, store, fetch, dir
+        Boolean isRegistered = false; // for store, fetch, dir
 
-                    userInput = scan.nextLine();
-
-                    String[] command = userInput.split("\\s+");
-                    if ("/join".equals(command[0])) { // Join server via ip and port
-
-                        if (command.length != 3) { // Command must have ip address and port as arguments
-                            errorString = "Error: Command parameters do not match or is not allowed.";
-                            break;
-                        }
-
-                        try { // Connect to server (valid: 127.0.0.1, 12345)
-                            socket = new Socket(command[1], Integer.valueOf(command[2]));
-                            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                            out = new PrintWriter(socket.getOutputStream(), true);
-                        } catch (Exception e) { // Incorrect IP and/or port number error
-                            errorString = "Error: Connection to the Server has failed! Please check IP Address and Port Number.";
-                            break;
-                        }
-
-                        out.println(userInput); // Send valid command to server
-                        System.out.println(in.readLine()); // Receive response from server
-
-                    } else if ("/leave".equals(command[0])) { // Leave server
-                        if (command.length != 1) { // Command must have only 1 argument
-                            errorString = "Error: Command parameters do not match or is not allowed.";
-                            break;
-                        }
-
-                        try {
-                            out.println(userInput); // Send valid command to server
-                            System.out.println(in.readLine()); // Receive response from server
-                            
-                            socket.close(); // Close connection
-                        } catch (Exception e) {
-                            System.out.println("Error: Disconnection failed. Please connect to the server first.");
-                            break;
-                        } 
-                    }  else if ("/?".equals(command[0])) { // View command list
-                        if (command.length == 1) {
-                            System.out.println("Available commands:");
-
-                            System.out.println(
-                                    "          /join <server_ip_add> <port>: Connect to the server application.");
-                            System.out.println("          /leave: Disconnect from the server application.");
-                            System.out.println("          /register <handle>: Register a unique handle or alias.");
-                            System.out.println("          /store <filename>: Send a file to the server.");
-                            System.out.println("          /dir: Request the directory file list from the server.");
-                            System.out.println("          /get <filename>: Fetch a file from the server.");
-                        } else {
-                            System.out.println("Error: Command parameters do not match or are not allowed.");
-                            break;
-                        }
-                    } else { // Unknown or wrong syntax error
-                        errorString = "Error: Command not found.";
-                        break;
-                    }
-
-                } while ((userInput != null));
-
+        Scanner scan = new Scanner(System.in);
+        try {
+            do {
                 // Print errors
                 if (!(errorString.isEmpty())) {
                     System.out.println(errorString);
                     errorString = "";
                 }
 
-                
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+                System.out.print(username + ": ");
+
+                scan = new Scanner(System.in);
+                userInput = scan.nextLine();
+
+                String[] command = userInput.split("\\s+");
+                if ("/join".equals(command[0])) { // Join server via ip and port
+
+                    if (command.length != 3) { // Command must have ip address and port as arguments
+                        errorString = "Error: Command parameters do not match or is not allowed.";
+                        continue;
+                    }
+
+                    try { // Connect to server (valid: 127.0.0.1, 12345)
+                        socket = new Socket(command[1], Integer.valueOf(command[2]));
+                        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                        out = new PrintWriter(socket.getOutputStream(), true);
+
+                    } catch (Exception e) { // Incorrect IP and/or port number error
+                        errorString = "Error: Connection to the Server has failed! Please check IP Address and Port Number.";
+                        continue;
+                    }
+
+                    out.println(userInput); // Send join command to server
+                    System.out.println(in.readLine()); // Receive response from server
+                    isConnected = true;
+
+                } else if ("/leave".equals(command[0])) { // Leave server
+                    if (command.length != 1) { // Command must have only 1 argument
+                        errorString = "Error: Command parameters do not match or is not allowed.";
+                        continue;
+                    }
+
+                    try {
+                        out.println(userInput); // Send leave command to server
+                        System.out.println(in.readLine()); // Receive response from server
+
+                        socket.close(); // Close connection
+                    } catch (Exception e) {
+                        errorString = "Error: Disconnection failed. Please connect to the server first.";
+                        continue;
+                    }
+                    isConnected = false;
+                } else if ("/register".equals(command[0])) { // Register client to server
+
+                    if (command.length != 2) { // Command must have only 2 arguments
+                        errorString = "Error: Command parameters do not match or is not allowed.";
+                        continue;
+                    }
+
+                    if(!isConnected){
+                        errorString = "Error: Disconnection failed. Please connect to the server first."; //TODO: misleading comment
+                        continue;
+                    }
+
+                    try {
+                        out.println(userInput); // Send register command to server
+                        System.out.println(in.readLine()); // Receive response from server
+
+                        isRegistered = true;
+                    } catch (Exception e) {
+                        System.out.println("Error: Registration failed. Handle or alias already exists. /c");
+                        continue;
+                    }
+
+                } else if ("/?".equals(command[0])) { // View command list
+                    if (command.length == 1) {
+                        System.out.println("Available commands:");
+
+                        System.out.println(
+                                "          /join <server_ip_add> <port>: Connect to the server application.");
+                        System.out.println("          /leave: Disconnect from the server application.");
+                        System.out.println("          /register <handle>: Register a unique handle or alias.");
+                        System.out.println("          /store <filename>: Send a file to the server.");
+                        System.out.println("          /dir: Request the directory file list from the server.");
+                        System.out.println("          /get <filename>: Fetch a file from the server.");
+                    } else {
+                        errorString = "Error: Command parameters do not match or are not allowed.";
+                    }
+                } else { // Unknown or wrong syntax error
+                    errorString = "Error: Command not found.";
+                }
+            } while (true);
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            scan.close();
         }
     }
 }
--- Server/Server.java
+++ Server/Server.java
@@ -7,8 +7,12 @@ import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Server {
+    private static Set<String> registeredUsernames = new HashSet<>();
+
     public static void main(String[] args) throws IOException {
         InetAddress serverAddress = InetAddress.getByName("127.0.0.1");
         int port = 12345;
@@ -39,6 +43,14 @@ public class Server {
                 out.println("You are disconnected. Goodbye!");
                 clientSocket.close(); // Close client's socket
                 break;
+            } else if ("/register".equals(request[0])) {
+                // Client wants to register
+                if(registeredUsernames.contains(request[1])){
+                    out.println("Error: Registration failed. Handle or alias already exists.");
+                } else {
+                    out.println("Welcome " + request[1] + "!");
+                    registeredUsernames.add(request[1]);
+                }
             } 
         }
 
